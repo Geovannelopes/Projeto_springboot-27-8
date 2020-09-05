01ec9d519b1f6dc1e0af655e0af063ca715bd15f
geovanne.lopes <geovanneblopes2@gmail.com>
2020-09-05 09:50:02 -0300

    #new - aula 03/09

---
+++ src/main/java/com/example/aula_pratica_2708/controller/Cliente.java
@@ -0,0 +1,9 @@
+package com.example.aula_pratica_2708.controller;
+
+public class Cliente {
+
+    public int codigo;
+    public String nome;
+    public String endereco;
+    public double saldo;
+}
--- src/main/java/com/example/aula_pratica_2708/controller/ClienteController.java
+++ src/main/java/com/example/aula_pratica_2708/controller/ClienteController.java
@@ -1,5 +1,10 @@
 package com.example.aula_pratica_2708.controller;
 
+import java.util.Arrays;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,18 +12,50 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class ClienteController {
 
+    List <Cliente> clientes;
+
+    @PostConstruct
+    public void criarClientes(){
+        Cliente c1 = new Cliente();
+        Cliente c2 = new Cliente();
+        Cliente c3 = new Cliente();
+
+        c1.codigo = 1;
+        c1.nome ="Jose";
+        c1.endereco ="Rua X, 99";
+        c1.saldo = 100;
+
+        c2.codigo = 2;
+        c2.nome ="Maria";
+        c2.endereco ="Rua Y, 123";
+        c2.saldo = 200;
+
+        c3.codigo = 3;
+        c3.nome ="Fernanda";
+        c3.endereco ="Rua Q, 25";
+        c3.saldo = 400;
+
+        clientes = Arrays.asList(c1,c2,c3);
+    }
+
+
     @GetMapping("/clientes")
-    public String getClientes(){
-        return "Algum dia vai retornar todos os clientes do BD!";
+    public List<Cliente> getClientes(){
+        
+        return clientes;
     }
 
     @GetMapping("/clientes/{codigo}")
-    public String getClientes1(@PathVariable int codigo){
-        if(codigo > 0)
-        return "Algum dia vai retornar o clientes do ID desejado do BD! (versao 2.0) - " + codigo;
-        else
-        return "Erro, codigo invalido " + codigo;
+    public Cliente getClientes1(@PathVariable int codigo){
+        Cliente cli =null;
 
+        for(Cliente aux: clientes){
+            if(aux.codigo == codigo){
+                cli = aux;
+                break;
+            }
+        }
+        return cli;
     }
 
 
